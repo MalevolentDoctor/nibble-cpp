8d90bca38593f9b31b40da70fe6a0441e382a3e1
malevolent_doctor <malevolentdoctor@gmail.com>
2024-02-05 11:20:43 +1000

    Reworked some enums

--- nibble/Helper.hpp
+++ nibble/Helper.hpp
@@ -8,11 +8,17 @@ enum ProgramStates {
 };
 
 enum ProgramModes {
-	PROGRAM_MODE_MAIN_MENU
+	PROGRAM_MODE_MAIN_MENU,		// On launch
+	PROGRAM_MODE_WORKBENCH	// On press start
 };
 
+enum DesktopMode {
+	DESKTOP_MODE_ACTIVE,
+	DESKTOP_MODE_INACTIVE
+};
 
 struct ProgramState {
 	int state = PROGRAM_STATE_STARTING;	// State of the program, running, suspended etc.
 	int mode = PROGRAM_MODE_MAIN_MENU;	// Mode of the program, what is it currently doing.
 };
+
